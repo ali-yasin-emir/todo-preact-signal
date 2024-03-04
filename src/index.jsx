88530745c707e0051d33660ff1d84c4352594e6d
reacton <aliyasin.emir16@gmail.com>
2024-03-04 13:23:02 +0300

    I add the localStorage feature to my app but still have some bugs to fix

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,21 @@
 import { render } from "preact";
 
 import "./style.css";
-import { batch, signal, useSignal } from "@preact/signals";
+import { batch, effect, signal } from "@preact/signals";
 
 const count = signal(0);
-const todos = signal([]);
+
+const LOCAL_STORAGE_KEY = ""
+
+const getTodos = () => {
+const value = localStorage.getItem(LOCAL_STORAGE_KEY)
+if(value == null) return []
+return JSON.parse(value)
+}
+
+const todos = signal(getTodos());
+
+
 const todo = signal("");
 const onEdit = signal(false);
 
@@ -13,11 +24,15 @@ const alertColor = signal("bg-red-700");
 
 const focus = signal(false);
 
-const done = signal(false);
-
 const randomColor = signal("#000000");
 
-// focus()
+effect(() => {
+// storing input name
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos.value));
+  console.log(todos.value)
+});
+
+
 const handleChange = (e) => {
   todo.value = e.target.value;
 };
@@ -45,6 +60,8 @@ const handleAdd = () => {
       });
       alert.value = "Todo is successfully added.";
       alertColor.value = "bg-green-700";
+     
+    /* 
       todos.value.push({
         id: crypto.randomUUID(),
         text: todo.value,
@@ -53,18 +70,20 @@ const handleAdd = () => {
         color: randomColor.value,
         focus: focus.value,
       });
-      /* x
-      todos.value = [
+
+    */
+   todos.value = [
         ...todos.value,
         {
           id: crypto.randomUUID(),
           text: todo.value,
-          onEdit: false,
-          completed: completed.value,
+          onEdit: false, // false
+          done: false,
           color: randomColor.value,
+          focus: focus.value,
         },
       ];
-      */
+      
     });
   }
 
@@ -84,46 +103,55 @@ const handleRename = (e, text) => {
   todos.value.find((todo) => todo.text === text).text = e.target.value;
 };
 
+const handleEdit = (id) => {
+  onEdit.value = !onEdit.value
+  console.log("clicked edit")
+  todos.value.find((todo) => todo.id === id).onEdit = onEdit.value;
+  focus.value = !focus.value;
+  console.log("todo onEdit: " + todos.value.find((todo) => todo.id === id).onEdit)
+  /* 
+    
+    todos.value = [
+       ...(todos.value.find((todo) => todo.id === id).onEdit = onEdit.value),
+     ];
+     [s
+      ...(todos.value.find((todo) => todo.id === id).onEdit = onEdit.value),
+    ];
+  
+  */
+  todos.value.find((todo) => todo.id === id).completed = false;
+  todos.value.find((todo) => todo.id === id).focus = focus.value;
+
+
+  /// TUM BUGLARIMIN DERMANI
+  todos.value = [...todos.value]
+
+};
+
+const handleComplete = (text, id) => {
+  console.log("clicked complete")
+
+  focus.value = !focus.value;
+  todos.value.find((todo) => todo.id === id).focus = false;
+  alert.value = "Todo successfully renamed";
+  alertColor.value = "bg-green-700";
+  if (text === "") {
+    alert.value = "Don't leave todo empty";
+    alertColor.value = "bg-red-700";
+    return;
+  }
+  todos.value.find((todo) => todo.id === id).text = text;
+  todos.value.find((todo) => todo.id === id).onEdit = false;
+  todos.value.find((todo) => todo.id === id).focus = false;
+
+  /// TUM BUGLARIMIN DERMANI
+  todos.value = [...todos.value]
+
+  
+};
 
 export function App() {
-  const handleEdit = (id) => {
-    todos.value.find(todo => todo.id === id).done = false
-    onEdit.value = !onEdit.value;
-    focus.value = !focus.value;
-    /* 
-      
-      todos.value = [
-         ...(todos.value.find((todo) => todo.id === id).onEdit = onEdit.value),
-       ];
-       [
-        ...(todos.value.find((todo) => todo.id === id).onEdit = onEdit.value),
-      ];
-    
-    */
-    todos.value.find((todo) => todo.id === id).onEdit = onEdit.value;
-    todos.value.find((todo) => todo.id === id).completed = false;
-    todos.value.find((todo) => todo.id === id).focus = focus.value;
-  };
 
-  const handleComplete = (text, id) => {
-    focus.value = !focus.value;
-    todos.value.find((todo) => todo.id === id).focus = false;
-    alert.value = "Todo successfully renamed";
-    alertColor.value = "bg-green-700";
-    if (text === "") {
-      alert.value = "Don't leave todo empty";
-      alertColor.value = "bg-red-700";
-      return;
-    }
-    onEdit.value = !onEdit.value;
-    todos.value.find((todo) => todo.id === id).text = text;
-    todos.value.find((todo) => todo.id === id).onEdit = false;
-    todos.value.find((todo) => todo.id === id).focus = false;
-  };
-  const handleDone = (id) => {
-    todos.value.find((todo) => todo.id === id).done = false;
-  };
-  
   return (
     <div className="flex flex-col gap-8 w-[540px]">
       <h1 className="self-center text-4xl">Todo</h1>
@@ -163,17 +191,15 @@ export function App() {
 
       <div className="flex flex-col gap-6">
         {todos.value.map((todo) => {
-          console.log(onEdit.value);
-          console.log(todo.color);
+          console.log(todos.value );
           return (
             <div
-              onClick={() => handleDone(todo.id)}
               key={todo.id}
               className={`bg-black flex justify-between gap-24 py-6 px-4 text-lg`}
             >
               <input
                 onChange={(e) => handleRename(e, todo.text)}
-                className={`${todo.done && "line-through"} ${
+                className={`${
                   todo.focus && "ring ring-slate-300"
                 } px-2 py-1 bg-transparent outline-none ${
                   // AFTER outline-none
